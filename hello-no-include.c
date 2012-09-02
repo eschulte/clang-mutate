22c496f204b50213777a96eb8278197921fe8f8f
Eric Schulte <eric.schulte@gmx.com>
2012-09-02 16:02:29 -0600

    adding an example of what *does* work

---
+++ hello-no-include.c
@@ -0,0 +1,19 @@
+int do_math(int input)
+{
+  int a,b;
+  // and do some math
+  a = input;
+  b = 1;
+  return a + b;  
+}
+
+int main(int argc, char *argv[])
+{
+  
+  // This is an IF statement w/o braces
+  if(argc == 1) puts("hello world.");
+  else          puts("be quiet!");
+  // Also, print out some other stuff
+  puts("other stuff");
+  return do_math(-1);
+}
